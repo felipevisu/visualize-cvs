5ed475154e5502d23ce76b1685318398bdfb5801
Felipe Faria <felipevisu@gmail.com>
2022-12-12 19:42:41 -0300

    first commit

--- graphqlstarter/settings.py
+++ graphqlstarter/settings.py
@@ -145,7 +145,7 @@ STATICFILES_FINDERS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
-    "SCHEMA": "portal.graphql.schema.schema",
+    "SCHEMA": "graphqlstarter.graphql.schema.schema",
     "SCHEMA_OUTPUT": "schema.graphql",
     "SCHEMA_INDENT": 2,
     "MIDDLEWARE": ["graphql_jwt.middleware.JSONWebTokenMiddleware"],
